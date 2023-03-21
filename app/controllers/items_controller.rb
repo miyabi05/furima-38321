406437abc33394c0c2a0280ce32c0a04532d49d6
miyabi05 <miyabi.m.0511@gmail.com>
2023-03-21 20:52:50 +0900

    商品情報編集機能の実装2

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-
+  before_action :set_item, only: [:edit, :show, :update]
   def index
     @items = Item.all.order('created_at DESC')
   end
@@ -19,17 +19,13 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
     redirect_to root_path unless current_user.id == @item.user_id
   end
 
   def update
-    @item = Item.find(params[:id])
-    @item.update(item_params)
     if @item.update(item_params)
       redirect_to item_path
     else
@@ -42,5 +38,9 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:content, :image, :items_name, :state_id, :region_id, :postage_id, :shipping_date_id,
                                  :category_id, :explain, :user_id, :price).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
