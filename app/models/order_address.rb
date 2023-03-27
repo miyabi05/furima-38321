c1ea8597faed5003fc373653cbd1ebc7fbaf9df7
miyabi05 <miyabi.m.0511@gmail.com>
2023-03-27 19:07:32 +0900

    商品購入機能の実装8

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -14,7 +14,7 @@ class OrderAddress
 
   def save
     purchase = Purchase.create(item_id: item_id, user_id: user_id)
-    ShippingAddress.create(post_code: post_code, region_id: region_id, city: city, address: address, building_name: building_name, phone_number: phone_number, purchase: purchase, token: token)
+    ShippingAddress.create(post_code: post_code, region_id: region_id, city: city, address: address, building_name: building_name, phone_number: phone_number, purchase: purchase, item: item, user: user)
   end
   end
 end
